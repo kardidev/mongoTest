53dd40c4bc70b337e0c2ef7b09fa775e1c8d7679
m.lysenchuk <m.lysenchuk@levi9.com>
2015-07-02 18:33:39 +0300

    First step

---
+++ src/main/java/com/kardi/test/mongo/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.kardi.test.mongo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String hello() {
+        return "index";
+    }
+}
